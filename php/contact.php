fc17d8488f76f0aac0bae93ac6290420b73f5f8f
DmitriyKovchugo <dkovchugo@mail.ru>
2021-03-06 00:00:05 +0300

    init: start portfolio

---
+++ php/contact.php
@@ -0,0 +1,54 @@
+<?php
+
+$post = (!empty($_POST)) ? true : false;
+
+if($post)
+{
+$email = trim($_POST['email']);
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$message = htmlspecialchars($_POST['message']);
+$error = '';
+
+if(!$name)
+{
+$error .= 'Пожалуйста введите ваше имя<br />';
+}
+
+// (length)
+if(!$message || strlen($message) < 1)
+{
+$error .= "Введите ваше сообщение<br />";
+}
+if(!$error)
+{
+
+
+$name_tema = "=?utf-8?b?". base64_encode($name) ."?=";
+
+$subject ="Новая заявка с сайта domain.name";
+$subject1 = "=?utf-8?b?". base64_encode($subject) ."?=";
+/*
+$message ="\n\nСообщение: ".$message."\n\nИмя: " .$name."\n\nТелефон: ".$tel."\n\n";
+*/
+$message1 ="\n\nИмя: ".$name."\n\nE-mail: " .$email."\n\nСообщение: ".$message."\n\n";	
+
+
+$header = "Content-Type: text/plain; charset=utf-8\n";
+
+$header .= "From: Новая заявка <dkovchugo@mail.ru>\n\n";	
+$mail = mail("dkovchugo@mail.ru", $subject1, iconv ('utf-8', 'windows-1251', $message1), iconv ('utf-8', 'windows-1251', $header));
+
+if($mail)
+{
+echo 'OK';
+}
+
+}
+else
+{
+echo '<div class="notification_error">'.$error.'</div>';
+}
+
+}
+?>
